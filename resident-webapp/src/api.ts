c76f9c4b30e8d00e7c017683bed667269fb5a746
Thushani Jayasekera <thushanij.18@cse.mrt.ac.lk>
2023-07-31 16:21:32 +0530

    config delete

--- resident-webapp/src/api.ts
+++ resident-webapp/src/api.ts
@@ -8,6 +8,6 @@ export function setAccessToken(token: string) {
 }
 
 export const API = axios.create({
-    baseURL: window.config.baseURL
+    baseURL: "https://2c6c3c6e-fa65-4e8d-8943-215214887ec8-dev.e1-us-east-azure.choreoapis.dev/knzw/thushanivisitapi/visit-420/1.0.0"
    
 });
